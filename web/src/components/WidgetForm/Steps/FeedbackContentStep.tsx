cc3bc5a5580e331c11b5850f2dfb99820a09e58a
Marcelo Lupatini <mar.lupatini@gmail.com>
2022-05-03 14:22:38 -0300

    Conteúdo e estilo do form. da etapa de conteúdo

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeft } from "phosphor-react";
+import { ArrowLeft, Camera } from "phosphor-react";
 
 import { feedbackTypes, FeedbackType } from "..";
 
@@ -32,13 +32,56 @@ export function FeedbackContentStep({
           <img
             src={feedbackTypeInfo.image.source}
             alt={feedbackTypeInfo.image.alt}
+            className="w-6 h-6"
           />
           {feedbackTypeInfo.title}
         </span>
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full" />
+      <form className="my-4 w-full">
+        <textarea
+          className="
+            min-w-[304px] w-full min-h-[112px]
+            text-sm placeholder-zinc-400 text-zinc-100
+            border-zinc-600 bg-transparent rounded-md
+            focus:border-brand-500 focus:ring-brand-500 focus:ring-1
+            focus:outline-none
+            resize-none
+            scrollbar scrollbar-thin
+            scrollbar-thumb-zinc-700 scrollbar-track-transparent
+          "
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+
+        <footer className="flex gap-2 mt-2">
+          <button
+            type="button"
+            className="
+              p-2 bg-zinc-800 rounded-md border-transparent
+              houver:bg-zinc-700 transition-colors
+              focus:outline-none
+              focus:ring-2 focus:ring-offset-2
+              focus:ring-offset-zinc-900 focus:ring-brand-500
+            "
+          >
+            <Camera className="w-6 h-6 text-zinc-100" />
+          </button>
+          <button
+            type="submit"
+            className="
+              p-2 bg-brand-500 rounded-md border-transparent
+              flex-1 justify-center items-center text-sm
+              hover:bg-brand-300 transition-colors
+              focus:outline-none
+              focus:ring-2 focus:ring-offset-2
+              focus:ring-offset-zinc-900 focus:ring-brand-500
+            "
+          >
+            Enviar feedback
+          </button>
+        </footer>
+      </form>
     </>
   );
 }
