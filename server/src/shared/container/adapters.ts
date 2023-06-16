d5468ea8429d254471bfffc65b0a47ba03bea8e3
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-06-16 19:49:18 -0300

    Remoção da escolha de email adapter por var. ambiente

--- server/src/shared/container/adapters.ts
+++ server/src/shared/container/adapters.ts
@@ -1,18 +1,9 @@
 import { container } from "tsyringe";
 
 import { IEmailAdapter } from "../../adapters/IEmailAdapter";
-import { MailtrapEmailAdapter } from "../../adapters/implementations/MailtrapEmailAdapter";
 import { GmailEmailAdapter } from "../../adapters/implementations/GmailEmailAdapter";
 
-const EmailAdapter = {
-  mailtrap: container.resolve(MailtrapEmailAdapter),
-  gmail: container.resolve(GmailEmailAdapter),
-};
-
-const chosenEmailAdapter =
-  process.env.EMAIL_PROVIDER as 'mailtrap' | 'gmail';
-
-container.registerInstance<IEmailAdapter>(
+container.registerSingleton<IEmailAdapter>(
   "EmailAdapter",
-  EmailAdapter[chosenEmailAdapter],
+  GmailEmailAdapter
 )
